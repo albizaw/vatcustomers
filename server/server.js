758b6aa07e6fce76f66a3be9901fb5f4c687f3d7
Albert <zawadaa10@gmail.com>
2023-04-04 10:45:35 +0200

    Edit operation in panel changed

--- server/server.js
+++ server/server.js
@@ -13,15 +13,6 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan('tiny'));
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
-
 const port = process.env.PORT || 8000;
 mongoose.connect(`${process.env.MONGO_DB}`, {
   useNewUrlParser: true,
@@ -33,6 +24,15 @@ app.get('/', (req, res) => {
 });
 
 app.use('/', router);
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error'));
